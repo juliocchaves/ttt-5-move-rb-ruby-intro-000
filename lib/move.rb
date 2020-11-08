807fef9620c34e4f9300369a2aebc39881fafb2a
juliochavesnyc <juliochavesnyc@gmail.com>
2020-11-08 02:43:45 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def update_array_at_with(board, index, value)
+def update_array_at_with(board, user_input, value)
   array[index] = value
 end
 
