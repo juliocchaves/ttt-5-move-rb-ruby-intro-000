6b5a124ff664a3fd4e01cd740502c500ddb3fd72
juliochavesnyc <juliochavesnyc@gmail.com>
2020-11-08 03:29:02 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,8 +12,8 @@ def input_to_index(user_input)
 end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
-def move(board, input, marker)
-  board[input] = marker
+def move(board, input, "X")
+  board[input] = "X"
 end
 
 move(board, 2)
